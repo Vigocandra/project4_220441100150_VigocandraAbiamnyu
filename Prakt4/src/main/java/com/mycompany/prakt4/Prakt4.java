0b6eb5e4dd0495021b8d5ffe6406d20a7c86069a
Vigo Candra Abimanyu <113223334+Vigocandra@users.noreply.github.com>
2023-10-25 14:05:47 +0700

    Add files via upload

---
+++ Prakt4/src/main/java/com/mycompany/prakt4/Prakt4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prakt4;
+
+/**
+ *
+ * @author BIMA
+ */
+public class Prakt4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
